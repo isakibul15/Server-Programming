742089234775039497c4827ae4651d08c4d8c7f3
isakibul15 <sakibulislam55@iut-dhaka.edu>
2022-10-24 00:05:34 +0600

    added app.js modified

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
 require("dotenv").config()
 const express = require("express")
-const bodyParser = require("body-parser")
-const router = require("./routes");
+const router = require("./routes")
 const mongoose = require("mongoose")
+const bodyParser = require("body-parser")
 
 database_url = process.env.DATABASE_URL
 
-mongoose.connect(database_url).then(()=>{
-    console.log("Database connected!")
-}).catch((error)=>{
-    console.log(error)
+mongoose.connect(database_url).then(() => {
+    console.log(`Database Connected.`)
+}).catch((err) => {
+    console.log(err)
 })
 
-const app = express()
 const port = 3000
+const app = express()
 
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 app.use(router)
+
 app.set("view engine", "ejs")
-app.set("views", __dirname+"/views")
+app.set("views",__dirname+"/views")
 
 app.listen(port, ()=>{
     console.log(`App is running at http://localhost:${port}`)
