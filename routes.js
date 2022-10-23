dae047fa373ecc620974c1a19a953ee8430fadb9
isakibul15 <sakibulislam55@iut-dhaka.edu>
2022-10-23 23:57:01 +0600

    Updated

--- routes.js
+++ routes.js
@@ -3,11 +3,13 @@ const router = express.Router()
 const homeController = require("./controllers/homeController")
 const bookController = require("./controllers/bookController")
 
-router.get("/", homeController.getHome)
-router.get("/books", bookController.getBook)
-router.post("/books", bookController.postBook)
+router.get('/', homeController.getHomePage)
+router.get('/home', homeController.getHomePage)
+router.get("/books", bookController.addBook)
 router.get("/book-list", bookController.getBookList)
-router.get('/delete-book',bookController.deleteBook)
-router.get('/edit-book',bookController.editBook)
+router.post('/books',bookController.postBook)
+router.get('/book-list/delete/:id', bookController.deleteBook)
+router.get('/book-list/update/:id',bookController.showUpdateBook)
+router.post('/books/update/:id',bookController.updateBook)
 
-module.exports = router;
+module.exports = router
